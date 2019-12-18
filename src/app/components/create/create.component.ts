115f976844a8887e87124d95ada8f7d2b2737edf
alcomcl <alex.cofre.martinez@gmail.com>
2019-12-18 10:52:31 -0300

    Creacion del formulario seccion Contact

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+/* Importamos el modelo y el servicio */
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
+
+  /* Cargamos el servicio dentro de la propiedad providers del decorador */
+  providers: [ProjectService]
 })
 export class CreateComponent implements OnInit {
+  // Creamos una propiedad que guardara el titulo de esta seccion
+    public title: string;
+  // Creamos una propiedad de tipo Project. Esto será el objeto que modificará el formulario
+    public project: Project;
 
-  constructor() { }
+
+  constructor(
+    // Creamos las propiedades del servicio
+      private _projectService: ProjectService
+  ) { 
+      this.title ="Crear Proyecto";
+    // Intanciamos el modelo Project y dejamos las propiedades vacías para luego ser llenadas por el formulario
+      this.project = new Project('','','','',2019,'','');
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(form){
+    console.log(this.project);
   }
 
 }
